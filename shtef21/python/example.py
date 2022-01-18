0e24367e96e8240261734416f364fd497a9a5b46
shtef21 <stjepan.salopek@hotmail.com>
2022-01-18 22:11:04 +0100

    extend train options

--- shtef21/python/example.py
+++ shtef21/python/example.py
@@ -1,10 +1,7 @@
 
 import train as shtef
-
-import torch
 from torch import nn
 from torch import optim
-from torch.utils.data import DataLoader as DL
 from torchvision import datasets 
 from torchvision import transforms as T
 
@@ -16,34 +13,12 @@ model = nn.Sequential(
     nn.Linear(28 * 28, 10)
 )
 
-def test():
-    train_loader = DL(
-        datasets.MNIST('', train=False, download=True, transform=T.ToTensor()),
-        batch_size=16,
-    )
-    correct, total = 0, 0
-    for images, labels in train_loader:
-        out = model(images)
-        preds = torch.argmax(out, -1)
-        correct += (preds == labels).sum().item()
-        total += len(labels)
-    return correct / total
-
-print(f"""
-    acc 1: {test()}
-    acc 2: {test()}
-""")
-
 shtef.train(
     model,
-    epochs=1,
+    epochs=3,
     criterion=nn.CrossEntropyLoss(),
     optimizer=optim.SGD(model.parameters(), 1e-3, 0.9),
     trainset=datasets.MNIST('', transform=T.ToTensor(), download=True),
+    testset=datasets.MNIST('', train=False, transform=T.ToTensor(), download=True),
+    test_once=True
 )
-
-print(f"""
-    acc 1: {test()}
-    acc 2: {test()}
-""")
-
--- shtef21/python/train.py
+++ shtef21/python/train.py
@@ -17,11 +17,13 @@ def train(model, **kwargs):
         (required) criterion -- loss function \n
         (required) optimizer -- model optimizer \n
         (required) trainset -- train dataset \n
+        testset -- if sent, tests the model post epoch \n
         device -- force a device (default: cuda if exist else cpu) \n
         batch_size -- train loader batch size (default: 256) \n
         lr_scheduler -- optional post-epoch scheduler \n
         batch_f -- runs post batch, receives batch stats \n
-        epoch_f -- runs post epoch, receives epoch stats
+        epoch_f -- runs post epoch, receives epoch stats \n
+        test_once -- if True, only tests model after last epoch
 
     """
 
@@ -44,7 +46,7 @@ def train(model, **kwargs):
 
     device = arg_opt('device', 'cuda' if torch.cuda.is_available() else 'cpu')
     batch_size = arg_opt('batch_size', default=256)
-    # batch_size_test = 16
+    batch_size_test = 16
     epoch, epochs = arg_opt('epoch', 0), arg_req('epochs')
 
     model = model.to(device)
@@ -52,16 +54,19 @@ def train(model, **kwargs):
     optimizer = arg_req('optimizer')
     lr_scheduler = arg_opt('lr_scheduler')
     trainset = arg_req('trainset')
-    # testset = arg_req('testset')
+    testset = arg_opt('testset')
+    has_test = testset != None
+    test_once = arg_opt('test_once', False)
 
     # Check params
     assert not any(missing_kwargs), f'Missing required args: {missing_kwargs}'
 
     train_loader = DL(trainset, batch_size, pin_memory=True, shuffle=True)
-    # test_loader = DL(testset, batch_size_test, pin_memory=True)
+    test_loader = DL(testset, batch_size_test, pin_memory=True) if has_test else None
 
     for e_curr in range(epoch, epochs):
 
+        model.train()
         loop = tqdm(enumerate(train_loader), total=len(train_loader))
         loop.set_description(f'e={e_curr + 1}/{epochs}')
         correct, total = 0, 0
@@ -81,7 +86,7 @@ def train(model, **kwargs):
             correct += corr
             total += tot
 
-            loop.set_postfix(acc=round(corr / total, 4))
+            loop.set_postfix(acc=round(corr / tot, 4))
 
             if batch_f != None:
                 batch_f({
@@ -106,6 +111,24 @@ def train(model, **kwargs):
                     'acc': correct / total,
                 },
             })
+
+        if has_test and (test_once == False or e_curr + 1 == epochs):
+            with torch.no_grad():
+
+                model.eval()
+                test_loop = tqdm(enumerate(test_loader), total=len(test_loader))
+                test_loop.set_description(f'Test e={e_curr + 1}')
+                correct, total = 0, 0
+
+                for idx, (images, labels) in test_loop:
+
+                    images, labels = images.to(device), labels.to(device)
+                    out = model(images)
+                    preds = torch.argmax(out, -1)
+                    correct += (preds == labels).sum().item()
+                    total += len(labels)
+                    
+                    test_loop.set_postfix(acc=round(correct / total, 4))
 
         # End of epoch
 
